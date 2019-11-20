27d7819e646061ced2693f82781a8d957820b9d4
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-11-19 21:58:39 -0800

    connect Models to App

--- proj3-flask/app.py
+++ proj3-flask/app.py
@@ -1,12 +1,11 @@
 # import flask
-from flask import Flask
+from flask import Flask, g
 
 # import resources
 
 
 # import models
-
-
+import models
 
 
 DEBUG = True
@@ -14,6 +13,18 @@ PORT = 8000
 
 app = Flask(__name__)
 
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
 
 
 # create routes
