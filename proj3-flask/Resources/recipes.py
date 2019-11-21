0984ddf094d7512dadc2c4f416362c64b207a948
benjamingomez0 <benjamingomez0@gmail.com>
2019-11-21 11:29:17 -0800

    get all recipe route begun

--- proj3-flask/Resources/recipes.py
+++ proj3-flask/Resources/recipes.py
@@ -3,6 +3,16 @@ from flask import Blueprint, jsonify, request
 from playhouse.shortcuts import model_to_dict
 
 recipe = Blueprint('recipes','recipe')
+#Show Route for all Recipes
+
+@recipe.route('/', methods=["GET"])
+def get_all_recipes():
+    try:
+        query = models.Recipe.select()
+        query.execute()
+        return jsonify(data=model_to_dict(query)),status={"code":201}, "message":"Success")
+    except models.DoesNotExist:
+        return jsonify(data={}, status={"code": 401, "message": "Error getting the bag"  })
 
 #Show Route for individual Recipes
 @recipe.route('/<id>', methods=["GET"])
