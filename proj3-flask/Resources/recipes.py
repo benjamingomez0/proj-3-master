0fbdb6881b8978f646dee71bb40a9103ab5bb5da
Jonathan Huynh <jonhuynh05@gmail.com>
2019-11-21 12:51:00 -0800

    test

--- proj3-flask/Resources/recipes.py
+++ proj3-flask/Resources/recipes.py
@@ -13,10 +13,11 @@ def get_all_recipes():
     except models.DoesNotExist:
         return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"})
 
-#Create Route for Recipes
-@recipe.route('/', methods=["POST"])
-def create_recipe():
-   payload = request.get_json()
-   recipe = models.Recipe.create(**payload)
-   recipe_dict = model_to_dict(recipe)
-   return jsonify(data=recipe_dict, status={"code": 201, "message": "Success"})
+#Show Route for individual Recipes
+@recipe.route('/<id>', methods=["GET"])
+def get_recipe(id):
+    try:
+        recipe = model_to_dict(models.Recipe.get_by_id(id))
+        return jsonify(data=recipe, status={"code":201, "message":"Succes"})
+    except models.DoesNotExist:
+        return jsonify(data={}, status={"code": 401, "message": "Error Retrieving Information"})
