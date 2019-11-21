83b102c56983500fb856efb9c6080d6f4f4f1808
Jonathan Huynh <jonhuynh05@gmail.com>
2019-11-21 09:33:29 -0800

    update

--- proj3-flask/Resources/users.py
+++ proj3-flask/Resources/users.py
@@ -28,9 +28,12 @@ def register():
 @user.route('/login', methods=["POST"])
 def login():
     payload = request.get_json()
+    print(payload)
     try:
         user = models.User.get(models.User.email==payload['email'])
         user_dict = model_to_dict(user)
+        print(user)
+        print(user_dict)
         if check_password_hash(user_dict['password'], payload['password']):
             del user_dict['password']
             login_user(user)
