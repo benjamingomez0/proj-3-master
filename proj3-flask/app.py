42f2a340831b80956da1d0387872a927fa859eba
benjamingomez0 <benjamingomez0@gmail.com>
2019-11-25 15:31:03 -0800

    prepared for heroku

--- proj3-flask/app.py
+++ proj3-flask/app.py
@@ -32,7 +32,7 @@ def after_request(response):
     g.db.close()
     return response
 # connect CORS to React
-CORS(user, origins=['http://localhost:3000, "https://hattrickfoods.herokuapp.com/"], supports_credentials=True)
+CORS(user, origins=['http://localhost:3000', "https://hattrickfoods.herokuapp.com/"], supports_credentials=True)
 CORS(recipe, origins=['http://localhost:3000',"https://hattrickfoods.herokuapp.com/"], supports_credentials=True)
 # register blueprints
 app.register_blueprint(user, url_prefix='/users')
